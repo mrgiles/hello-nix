fb54ccec88eda12f99a1c077724dee6bd9dea402
Marcelo Giles <marcelogiles@gmail.com>
2024-04-21 14:15:37 -0700

    add flake.lock

--- flake.nix
+++ flake.nix
@@ -5,12 +5,12 @@
 
   outputs = { self, nixpkgs }: {
 
-    defaultPackage.x86_64-linux =
+    packages.x86_64-linux.default =
       # Notice the reference to nixpkgs here.
       with import nixpkgs { system = "x86_64-linux"; };
       stdenv.mkDerivation {
         name = "hello-nix";
-        xsrc = self;
+        src = self ;
         buildPhase = "gcc -o hello-nix ./hello-nix.c";
         installPhase = "mkdir -p $out/bin; install -t $out/bin hello-nix";
       };
